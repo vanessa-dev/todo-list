00e8bce6aa6763de6442dfdd3d011821faf56d47
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-06-23 17:09:56 -0300

    chore: adicionando tarefas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const handleChange = ({target, which}) => {
+    if (which === 13) {
+      setTasks([...tasks, target.value]);
+      target.value = "";
+    }
+  }
   return (
     <article className="App">
       <h1>Todo List</h1>
       <section className="card">
         <header>
           <input type="checkbox"/>
-          <input type="text" placeholder="What needs to be done?"/>
+          <input type="text"   onKeyPress={handleChange} placeholder="What needs to be done?"/>
         </header>
-        <div className="card--info">
-          <span>2 items left</span>
-          <span className="card--badge active">all</span>
-          <span className="card--badge">active</span>
-          <span className="card--badge">completed</span>
-        </div>
-        <div className="card--tasks">
-          <div className="task">
-            <input type="checkbox" id="teste" />
-            <label htmlFor="teste">teste</label>
-            <button>X</button>
+  
+        {
+          tasks.length > 0  && 
+          <div className="card--info">
+            <span>2 items left</span>
+            <span className="card--badge active">all</span>
+            <span className="card--badge">active</span>
+            <span className="card--badge">completed</span>
           </div>
+        } 
+        <div className="card--tasks">
+        {tasks.map((item, index) => (
+          <div className="task" key={index}>
+           <input type="checkbox" id={index}/>
+           <label htmlFor={index}>{item}</label>
+           <button>X</button>
+          </div>
+        ))}
         </div>
       </section>
     </article>
